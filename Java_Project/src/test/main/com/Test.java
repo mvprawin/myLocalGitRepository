6226c7eea6cfdbccf16ed733f0c42c0fcdb11bf5
mvprawin <prawin.mattool07@gmail.com>
2020-04-06 11:08:42 +0530

    Initial commit

---
+++ Java_Project/src/test/main/com/Test.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+/**
+ * @author mvprawin
+ *
+ */
+package test.main.com;
+
+import java.util.*;
+
+
+public class Test {
+
+	public void main(String Args[]) {
+		System.out.println("Testing Java project");
+	}
+	
+}
