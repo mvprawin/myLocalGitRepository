d6bdc955d2a409bb2f4f9be6321508fb7e830eb3
mvprawin <prawin.mattool07@gmail.com>
2021-04-06 23:17:34 +0530

    updated

--- CucumberJunitReport/src/test/java/com/qa/test/TestRunner.java
+++ CucumberJunitReport/src/test/java/com/qa/test/TestRunner.java
@@ -15,7 +15,7 @@ import cucumber.api.junit.Cucumber;
 	    features="src/test/resources/Features",
 	    glue="stepDefinitions",
 	   // plugin= {"pretty","html:target/site/cucumber-report-html","json:target/cucumber-report.json"},
-	   plugin= {"pretty","html:target/cucumber","json:target/cucumber.json",
+	   plugin= {"pretty","html:target/cucumber","json:target/cucumber/cucumber.json",
 			   "com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:target/extent-reports/report.html"},
 	    monochrome=true
 	    )
